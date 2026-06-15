5a01dd6f5a07234b67da5d40591a8f0248ad8ce7
eric0972809857-del <eric0972809857@gmail.com>
2026-06-15 13:44:53 +0800

    Update 15.user_add.php

--- 商用程式設計期末報告/15.user_add.php
+++ 商用程式設計期末報告/15.user_add.php
@@ -1,25 +1,57 @@
 <?php
 
+// 關閉錯誤訊息顯示
 error_reporting(0);
+
+// 啟動 Session，讀取登入資訊
 session_start();
+
+// 檢查使用者是否已登入
 if (!$_SESSION["id"]) {
+
+    // 未登入時顯示提示訊息
     echo "請登入帳號";
+
+    // 3 秒後跳轉到登入頁面
     echo "<meta http-equiv=REFRESH content='3, url=2.login.html'>";
 }
-else{    
+else {
 
-   #mysqli_connect() 建立資料庫連結
-   $conn=mysqli_connect("120.105.96.90", "immust", "immustimmust", "immust");
-   #mysqli_query() 從資料庫查詢資料
-   #新增資料SQL命令：insert into 表格名稱(欄位1,欄位2) values(欄位1的值,欄位2的值)
-   $sql="insert into user(id,pwd) values('{$_POST['id']}', '{$_POST['pwd']}')";
-   #echo $sql;
+   // 建立 MySQL 資料庫連線
+   // 參數依序為：主機位置、帳號、密碼、資料庫名稱
+   $conn = mysqli_connect(
+       "120.105.96.90",
+       "immust",
+       "immustimmust",
+       "immust"
+   );
+
+   // 建立新增資料的 SQL 指令
+   // 將表單送來的 id、pwd 寫入 user 資料表
+   // SQL 語法：
+   // INSERT INTO 表格名稱(欄位1, 欄位2)
+   // VALUES(值1, 值2)
+   $sql = "insert into user(id,pwd)
+           values('{$_POST['id']}',
+                  '{$_POST['pwd']}')";
+
+   // 顯示 SQL 指令
+   // echo $sql;
+
+   // 執行 SQL 新增指令
    if (!mysqli_query($conn, $sql)) {
-     echo "新增命令錯誤";
-   }
-   else{
-     echo "新增使用者成功，三秒鐘後回到網頁";
-     echo "<meta http-equiv=REFRESH content='3, url=18.user.php'>";
+
+        // 新增失敗
+        echo "新增命令錯誤";
+
+   } else {
+
+        // 新增成功
+        echo "新增使用者成功，三秒鐘後回到網頁";
+
+        // 3 秒後跳轉回使用者管理頁面
+        echo "<meta http-equiv=REFRESH content='3, url=18.user.php'>";
    }
 }
+
 ?>
